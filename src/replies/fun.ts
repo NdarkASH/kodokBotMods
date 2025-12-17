5b2d91744b50f29800dba568e367b97f29c90143
Darknash <nashihulwan9@gmail.com>
2025-12-17 18:38:56 +0700

    fun fitur

--- src/replies/fun.ts
+++ src/replies/fun.ts
@@ -14,8 +14,7 @@ export default {
 
         const gifs = [
             "https://media.tenor.com/2roX3uxz_68AAAAC/anime-head-pat.gif",
-            "https://media.tenor.com/7xr8r0nV5zEAAAAC/pat-anime.gif",
-            "https://media.tenor.com/Ii6G6ZqN6nQAAAAC/anime-pat.gif"
+            "https://tenor.com/view/vtuber-anime-shylily-bao-foxplushy-gif-6851130087983678188",
         ];
 
         const gif = gifs[Math.floor(Math.random() * gifs.length)];
